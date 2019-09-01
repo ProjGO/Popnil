241c9d5652900859601fe83ccce5ac182d02587c
heavenbirdxdx <1075481183@qq.com>
2019-09-01 22:08:02 +0800

    好友列表

--- client/list.c
+++ client/list.c
@@ -5,8 +5,15 @@ void list()
 {
     GtkWidget *window;
     GtkWidget *table;
+    GtkWidget *table_user;
     GtkWidget *image_usericon;
     GtkWidget *label_username;
+    GtkWidget *search_entry;
+    GtkWidget *notebook;
+    GtkWidget *page1;
+    GtkWidget *notebook_image_friend;
+    GtkWidget *page2;
+    GtkWidget *notebook_image_group;
 
 //    gtk_init(&argc, &argv);
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
@@ -15,18 +22,40 @@ void list()
     gtk_window_set_title(GTK_WINDOW(window),"Chat");
     gtk_container_set_border_width(GTK_CONTAINER(window),0);
 
-    table = gtk_table_new(10, 4, TRUE);
+    table = gtk_table_new(40, 16, TRUE);
     gtk_table_set_row_spacings(GTK_TABLE(table),0);
     gtk_table_set_col_spacings(GTK_TABLE(table),1);
     gtk_container_add(GTK_CONTAINER(window),table);
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+//  g_signal_connect(GTK_WINDOW(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+
+
 
     image_usericon = gtk_image_new_from_file("../client/images/friend_portrait.png");
-    gtk_table_attach_defaults(GTK_TABLE(table), image_usericon, 0, 1, 0, 1);
+    gtk_table_attach_defaults(GTK_TABLE(table), image_usericon, 1, 5, 1, 5);
 
     label_username = gtk_label_new("XDX");
-    gtk_table_attach_defaults(GTK_TABLE(table),label_username, 1, 3, 0, 1);
+    gtk_table_attach_defaults(GTK_TABLE(table),label_username, 5, 13, 1, 5);
 
+    search_entry = gtk_search_entry_new();
+    gtk_table_attach_defaults(GTK_TABLE(table),search_entry, 0, 16, 6, 8);
+
+    gtk_entry_set_text(search_entry, "Search...");
+
+
+    notebook = gtk_notebook_new();
+    gtk_table_attach_defaults(GTK_TABLE(table), notebook, 0, 16, 9, 37);
+    notebook_image_friend = gtk_image_new_from_file("../client/images/friends_list.png");
+    notebook_image_group = gtk_image_new_from_file("../client/images/group_list.png");
+
+    page1 = gtk_hbox_new(FALSE, 0);
+    gtk_notebook_append_page(GTK_NOTEBOOK(notebook), page1, notebook_image_friend);
+
+    page2 = gtk_hbox_new(FALSE, 0);
+    gtk_notebook_append_page(GTK_NOTEBOOK(notebook), page2, notebook_image_group);
+
+    gtk_notebook_set_tab_reorderable(notebook, page1, TRUE);
+    gtk_notebook_set_tab_reorderable(notebook, page2, TRUE);
 
     gtk_widget_show_all(window);
     gtk_main();
