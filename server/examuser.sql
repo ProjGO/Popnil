8903aa27893866dd41c70f166698982dc76ea7b7
1120170478biteducn <1120170478@bit.edu.cn>
2019-09-02 09:02:45 -0400

    Add some friends.

--- server/examuser.sql
+++ server/examuser.sql
@@ -1,3 +1,9 @@
 insert into `userinfo` values (0, 'Kaslana', 'Kiana', 2, 'Milady.', curdate(), 0, 0);
 insert into `userinfo` values (1, 'Raiden', 'Mei', 3, 'Thunder.', curdate(), 0, 0);
 insert into `userinfo` values (2, 'Zaychik', 'Bronya', 5, 'Rabbit.', curdate(), 0, 0);
+
+insert into `userinfo` values (3, 'Apocalypse', 'Theresa', 6, 'The cutest.', curdate(), 0, 0);
+insert into `userinfo` values (4, 'Fu', 'Hua', 7, 'Give me, please.', curdate(), 0, 0);
+insert into `userinfo` values (5, 'Murata', 'Himeko', 1, 'Wait your order.', curdate(), 0, 0);
+insert into `userinfo` values (6, 'Rossweisse', 'Rita', 4, 'Serve you.', curdate(), 0, 0);
+insert into `userinfo` values (7, 'Vollerei', 'Seele', 8, 'Play with me!', curdate(), 0, 0);
