1e510bf19578883d8c436823d260a48523a7104a
1120170478biteducn <1120170478@bit.edu.cn>
2019-09-02 05:12:57 -0400

    The SQLs has been updated.

---
+++ server/examuser.sql
@@ -0,0 +1,3 @@
+insert into `userinfo` values (0, 'Kaslana', 'Kiana', 'Milady.', curdate(), 0, 0);
+insert into `userinfo` values (1, 'Raiden', 'Mei', 3, 'Thunder.', curdate(), 0, 0);
+insert into `userinfo` values (2, 'Zaychik', 'Bronya', 5, 'Rabbit.', curdate(), 0, 0);
