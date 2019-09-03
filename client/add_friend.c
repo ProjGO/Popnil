e12702785dc2e5a8c47b884655e6d7cfe73bbfb4
bigwei <1026819356@qq.com>
2019-09-03 16:45:37 +0800

    Merge branch 'master' of https://github.com/ProjGO/Linpop
    # Conflicts:
    #	client/list.c
    #	server/main.c

--- client/add_friend.c
+++ client/add_friend.c
@@ -3,90 +3,142 @@
 //
 
 #include <gtk/gtk.h>
-//#include <gtk-3.0>
 #include <gdk/gdkkeysyms.h>   //键盘头文件，GDK_Up在这声明
 #include "../common/include/include.h"
 #include "../common/include/define.h"
+GtkWidget* id;
+GtkWidget* user_name;
 GtkWidget *search_entry;
+GtkWidget* frame;
+extern int fd_log,fd_chat,fd_file;
 char* default_image_path = "../client/images/friend_portrait.png";
 char* default_id = "1120173454";
 char* default_name = "xdx";
 extern GtkWidget *create_button(char *image_path, char *button_label);
-extern void update_widget_bg(GtkWidget *widget, int w,int h, const gchar *img_file);
-
-GtkWidget *add_friend_window;
+void add()
+{
+    GtkWidget* dialog ;
+    GtkMessageType type ;
+    gchar *message;
+    message = "添加好友成功";
+    type = GTK_MESSAGE_INFO ;
+    dialog = gtk_message_dialog_new(NULL,
+                                    GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT, type ,
+            GTK_BUTTONS_OK,
+            message);
+    gtk_dialog_run(GTK_DIALOG(dialog));
+    gtk_widget_destroy(dialog);
+    oper_friend_info * msg=(oper_friend_info*)malloc(sizeof(oper_friend_info));
+    msg->id_re=atoi(gtk_label_get_text(id));
+    int flag=ADD_FRIEND;
+    if (write(fd_log, &flag, sizeof(int)) == -1 )
+    {
+        printf ("Error in send\n");
+        exit(1);
+    }
+    if (write(fd_log, msg, sizeof(oper_friend_info)) == -1 )
+    {
+        printf ("Error in send\n");
+        exit(1);
+    }
+    free(msg);
+}
 void search(GtkWidget *window, gpointer data)
 {
     oper_friend_info *id_text=(oper_friend_info*)malloc(sizeof(oper_friend_info));
     id_text->id_re=atoi(gtk_entry_get_text(GTK_ENTRY((GtkWidget *) search_entry)));
-
-
+    int flag;
+    flag=2;
+//    write(fd_log,&flag,sizeof(int));
+//    write(fd_log,id_text,sizeof(oper_friend_info));
+    if (write(fd_log, &flag, sizeof(int)) == -1 )
+    {
+        printf ("Error in send\n");
+        exit(1);
+    }
+    if (write(fd_log, id_text, sizeof(oper_friend_info)) == -1 )
+    {
+        printf ("Error in send\n");
+        exit(1);
+    }
+    client_info *msg=(client_info*)malloc(sizeof(client_info));
+    read(fd_log,msg, sizeof(client_info));
+    if(msg->id==-1)
+    {
+        //显示查无此人
+    }
+    else
+    {
+        gtk_label_set_text(user_name, msg->nickname);
+        printf("%s\n", user_name);
+        char str_id[24];
+        sprintf(str_id, "%d", msg->id);
+        gtk_label_set_text(id, str_id);
+        free(msg);
+    }
+//    gtk_widget_show_all(window);
 }
 
 GtkWidget *information_frame()
 {
-    GtkWidget* frame;
     GtkWidget* table;
     GtkWidget* image;
-    GtkWidget* id;
-    GtkWidget* user_name;
-    GtkWidget* button;
 
+    GtkWidget* button;
     frame = gtk_frame_new("Information");
     table = gtk_table_new(16, 12, TRUE);
     gtk_container_add(frame, table);
     gtk_frame_set_label(frame, "Information");
+
     image = gtk_image_new_from_file(default_image_path);
     gtk_table_attach_defaults(GTK_TABLE(table),image, 0, 5, 0, 5);
     id = gtk_label_new(default_id);
     user_name = gtk_label_new(default_name);
+
     gtk_table_attach_defaults(GTK_TABLE(table), id, 5, 10, 0, 3);
+
     gtk_table_attach_defaults(GTK_TABLE(table), user_name, 5, 10, 3, 5);
 
     button = create_button("../client/images/add.png", "添加");
     gtk_table_attach_defaults(GTK_TABLE(table), button, 5, 7, 9, 12);
+    g_signal_connect(G_OBJECT(button),"clicked",G_CALLBACK(add), NULL);
 
     return frame;
 }
 
 GtkWidget *add_friends()
 {
-
+    GtkWidget *window;
     GtkWidget *table;
     GtkWidget *frame;
     GtkWidget *button_search;
+    client_info* msgback=(client_info*)malloc(sizeof(client_info));
 
-    add_friend_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_position(GTK_WINDOW(add_friend_window),GTK_WIN_POS_CENTER);
-    gtk_window_set_default_size(GTK_WINDOW(add_friend_window),400,400);
-    gtk_window_set_title(GTK_WINDOW(add_friend_window),"Add friends");
-    gtk_container_set_border_width(GTK_CONTAINER(add_friend_window),0);
-    g_signal_connect ( add_friend_window, "destroy",
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_position(GTK_WINDOW(window),GTK_WIN_POS_CENTER);
+    gtk_window_set_default_size(GTK_WINDOW(window),400,400);
+    gtk_window_set_title(GTK_WINDOW(window),"Add friends");
+    gtk_container_set_border_width(GTK_CONTAINER(window),0);
+    g_signal_connect ( window, "destroy",
                        G_CALLBACK (gtk_main_quit), NULL);	//为窗口连接“退出事件”
     table = gtk_table_new(20, 20, TRUE);
     gtk_table_set_row_spacings(GTK_TABLE(table),0);
     gtk_table_set_col_spacings(GTK_TABLE(table),1);
-    gtk_container_add(GTK_CONTAINER(add_friend_window),table);
+    gtk_container_add(GTK_CONTAINER(window),table);
 
-//    search_entry = gtk_search_entry_new();
-    GdkPixbuf * pixbuf= gdk_pixbuf_new_from_file("../client/images/search.png", FALSE);
     search_entry = gtk_entry_new();
-    gtk_entry_set_has_frame(search_entry, FALSE);
-    gtk_entry_set_icon_from_pixbuf(search_entry, GTK_ENTRY_ICON_PRIMARY, pixbuf);
-    gtk_entry_set_text(search_entry, "Search...");
     gtk_table_attach_defaults(GTK_TABLE(table),search_entry, 2, 16, 2, 4);
 //    search_friends();
     button_search = create_button("../client/images/search.png", NULL);
     gtk_table_attach_defaults(GTK_TABLE(table),button_search, 17, 19, 2, 4);
     g_signal_connect ( button_search, "clicked",
-                       G_CALLBACK (search), NULL);
-
-    frame = information_frame();
+                       G_CALLBACK (search), msgback);
+    frame=information_frame();
     gtk_table_attach_defaults(GTK_TABLE(table),frame, 2, 18, 6, 18);
-    update_widget_bg(add_friend_window,400, 400, "../client/images/bg/7.jpg");
 
 
-    gtk_widget_show_all(add_friend_window);
+    gtk_widget_show_all(window);
     gtk_main();
-    return add_friend_window;
+    free(msgback);
+    return window;
 }
